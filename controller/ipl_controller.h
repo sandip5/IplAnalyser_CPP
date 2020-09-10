05b947ffe965758e43c860ce7e6ba9404da00311
“sandip5” <singhsandeep.singh97@gmail.com>
2020-09-10 14:57:53 +0530

    ADDED- Show Welcome Message In Controller Class

---
+++ controller/ipl_controller.h
@@ -0,0 +1,12 @@
+#include <iostream>
+
+class ipl_controller
+{
+public:
+    void show_welcome_message();
+};
+
+void ipl_controller::show_welcome_message()
+{
+    std::cout << "Welcome To Ipl Analyser System" << std::endl;   
+}
---
+++ main/main.cpp
@@ -0,0 +1,8 @@
+#include "../controller/ipl_controller.h"
+
+int main()
+{
+    ipl_controller controller;
+    controller.show_welcome_message();
+    return 0;
+}
