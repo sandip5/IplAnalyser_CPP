841ee496d4a5390b4e62fbdd1d8ed2964a2a2277
“sandip5” <singhsandeep.singh97@gmail.com>
2020-09-10 22:01:10 +0530

    WIP

--- main/main.cpp
+++ main/main.cpp
@@ -1,8 +1,20 @@
+#include "../view/ipl_view.h"
+#include "../model/ipl_analyser.h"
 #include "../controller/ipl_controller.h"
+
+template<typename T>
+void start_analyser()
+{
+    ipl_view view;
+    ipl_analyser<T> analyser;
+    ipl_controller<T> controller(view, analyser);
+    controller.show_welcome_message();
+    controller.display_top_batting_avg("Average");
+}
+
 
 int main()
 {
-    ipl_controller controller;
-    controller.show_welcome_message();
+    start_analyser<class ipl_run>();
     return 0;
 }
